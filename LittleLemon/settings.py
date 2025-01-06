a971937d4be1284c9f0553ef4c74d0cb2276711a
Beatriz Acosta <beatrizzaacosta29@gmail.com>
2025-01-06 01:43:51 -0800

    Includes Django Webframework setup with MySQl as Database. Models and corresponding serializers have been created.

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'Restaurant',
 ]
 
@@ -76,10 +77,13 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': r'C:\Program Files\MySQL\MySQL Server 8.0\my.ini',
+        },
     }
 }
+
 
 
 # Password validation
--- Restaurant/views.py
+++ Restaurant/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics, viewsets, permissions
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer, UserSerializer
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
